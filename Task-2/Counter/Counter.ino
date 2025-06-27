71092f8053502062f681e9bfc79a0a7fbbe945c6
atharvaMessi10 <148075001+atharvaMessi10@users.noreply.github.com>
2025-06-28 00:16:27 +0530

    Update Counter.ino

--- Task-2/Counter/Counter.ino
+++ Task-2/Counter/Counter.ino
@@ -1,1 +1,19 @@
+const int ledPins[4] = {8, 9, 10, 11}; 
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int count = 0; count < 16; count++) {
+   
+    for (int i = 0; i < 4; i++) {
+      bool bit = (count >> i) & 0x01;
+      digitalWrite(ledPins[i], bit);
+    }
+    delay(300); 
+  }
+}
 
